ff06dec42881a11c5ab3e1e2457d0baaf191b282
m.kokodei <m.kokodei@othapp.com>
2024-08-24 15:12:11 +0200

    add sqlQueryFormat function

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = function () {
+const pgPromise = require('pg-promise');
+const { format: sqlFormatter } = require('sql-formatter');
+const { highlight } = require('sql-highlight');
 
+const pgp = pgPromise();
+
+function sqlQueryFormat(query, params, prettyCfg) {
+  prettyCfg = prettyCfg || { language: 'postgresql' };
+
+  const formattedQuery = pgp.as.format(query, params);
+  const prettyQuery = sqlFormatter(formattedQuery, prettyCfg);
+  const highlightedQuery = highlight(prettyQuery);
+
+  return highlightedQuery;
 }
+
+module.exports = sqlQueryFormat;
